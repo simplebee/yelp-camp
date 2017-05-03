140d3b7f42806a2fac0dd85ac1e341b6f6c2760d
Shuiloon Yip <shuoodle@gmail.com>
2017-05-03 16:18:21 +0100

    Add user and campground association

--- routes/campground.js
+++ routes/campground.js
@@ -11,20 +11,14 @@ router.get('/', function(req, res) {
 });
 
 // New
-router.get('/new', function(req, res) {
+router.get('/new', isLoggedIn, function(req, res) {
   res.render('campground/new');
 });
 
 // Create
-router.post('/', function(req, res) {
-  var name = req.body.name;
-  var image = req.body.image;
-  var description = req.body.description;
-  var newCampground = {
-    name: name,
-    image: image,
-    description: description
-  };
+router.post('/', isLoggedIn, function(req, res) {
+  var newCampground = req.body.campground;
+  newCampground.author = req.user;
   Campground.create(newCampground, function(err) {
     if (err) return console.error(err);
     res.redirect('/campground');
@@ -42,5 +36,13 @@ router.get('/:id', function(req, res) {
       res.render('campground/show', {campgroundData: campgroundData});
     });
 });
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+}
 
 module.exports = router;
